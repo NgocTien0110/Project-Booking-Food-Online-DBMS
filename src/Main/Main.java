33674a60a92f2175a82cf0d616e58da3207e9345
Nguyễn Quốc Tuấn <81601059+NguyenQuocTuan385@users.noreply.github.com>
2022-12-13 15:48:40 +0700

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,14 +1,22 @@
 package Main;
 
+import DAO.*;
+import Model.*;
 import database.JDBCUtil;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = JDBCUtil.getConnection();
-        JDBCUtil.closeConnection(connection);
-        System.out.println(connection);
-
+        ArrayList<ThucDonModel> list = ThucDonDAO.getInstance().selectAll();
+        for(ThucDonModel chiNhanhModel : list) {
+            System.out.println(chiNhanhModel.toString());
+        }
+//        ChiNhanhModel find = new ChiNhanhModel();
+//        find.setMaChiNhanh("CN000001");
+//        find.setMaDoiTac("DT000001");
+//        ChiNhanhModel cn2 = ChiNhanhDAO.getInstance().selectById(find);
+//        System.out.println(cn2);
     }
 }
