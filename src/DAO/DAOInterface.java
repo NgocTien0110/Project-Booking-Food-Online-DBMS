607b07c7a70478e49682d01aa8b1c5bf3c56d262
NguyenQuocTuan385 <nguyenquoctuan385@gmail.com>
2022-12-13 17:10:46 +0700

    Cập nhật lại logic file

--- src/DAO/DAOInterface.java
+++ src/DAO/DAOInterface.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public interface DAOInterface<T>{
     public ArrayList<T> selectAll();
-    public int selectById(T t);
+    public T selectById(T t);
     public ArrayList<T> selectByCondition(String condition);
 }
