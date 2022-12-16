db655c06b17b9f3841c8fc9133fd3edc45ab7612
Đặng Ngọc Tiến <20127641@student.hcmus.edu.vn>
2022-12-16 17:51:11 +0700

    thêm file test demo

---
+++ src/Main/test.java
@@ -0,0 +1,22 @@
+package Main;
+
+import StoredProcedure.CallStoredProcedure;
+import StoredProcedure.XEM_MON_AN;
+import View.MenuDangNhapDangKy;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Main
+ * Create by Đặng Ngọc Tiến
+ * Date 12/14/2022 - 9:25 PM
+ * Description: ...
+ */
+public class test {
+    public static void main(String[] args) {
+        new MenuDangNhapDangKy();
+
+    }
+
+
+}
